8cc3e1b1d1fd37b367481d0f6933a11198fde1cc
Yan Verin <iliarkhpv@yandex.ru>
2024-05-27 09:58:29 +0300

    add 2 tasks

---
+++ week2_maps/task2_count_voices.go
@@ -0,0 +1,41 @@
+package week2_maps
+
+import (
+	"fmt"
+	"sort"
+)
+
+/* 2. Подсчет голосов.
+Напишите функцию подсчета каждого голоса за кандидата. Входной аргумент - массив с именами кандидатов.
+Результативный - массив структуры Candidate, отсортированный по убыванию количества голосов.
+Пример.
+Вход: ["Ann", "Kate", "Peter", "Kate", "Ann", "Ann", "Helen"]
+Вывод: [{Ann, 3}, {Kate, 2}, {Peter, 1}, {Helen, 1}]
+*/
+
+type Candidate struct {
+	Name  string
+	Votes int
+}
+
+func CountVoices(names []string) []Candidate {
+	countedVoices := make(map[string]int)
+	for _, name := range names {
+		countedVoices[name]++
+	}
+
+	var candidates []Candidate
+	fmt.Println(candidates)
+	for name, voices := range countedVoices {
+		candidates = append(candidates, Candidate{
+			Name:  name,
+			Votes: voices,
+		})
+	}
+
+	sort.Slice(candidates, func(i, j int) bool {
+		return candidates[i].Votes > candidates[j].Votes
+	})
+
+	return candidates
+}
