4c75467a6d21aed97c1192d2915ba72851129792
Yan Verin <iliarkhpv@yandex.ru>
2024-06-15 21:13:04 +0300

    add task 1 from 3week hometask(add channels)

---
+++ main.go
@@ -0,0 +1,10 @@
+package main
+
+import (
+	"practice/week3_multithreaing"
+)
+
+func main() {
+
+	week3_multithreaing.CmdLineReader()
+}
--- week3_multithreaing/task1_cl_parser.go
+++ week3_multithreaing/task1_cl_parser.go
@@ -5,46 +5,57 @@ import (
 	"log"
 	"os"
 	"os/signal"
+	"sync"
 )
 
-func CmdLineReader() []string {
-	var list []string
-	var a string
-
-	go func() {
-		sigchan := make(chan os.Signal)
-		signal.Notify(sigchan, os.Interrupt)
-		<-sigchan
-		log.Println("Считывание остановлено.")
-		FileWriter(list)
-		os.Exit(0)
-	}()
-	for {
-		fmt.Scan(&a)
-		list = append(list, a)
-	}
-
-	return list
-}
-
-func FileWriter(data []string) {
-
+func CmdLineReader() {
 	f, _ := os.Create("file.txt")
 	defer f.Close()
 
-	for _, i := range data {
-		f.WriteString(i + "\n")
+	readChan := make(chan string)
+	sigchan := make(chan os.Signal)
+	mu := new(sync.Mutex)
+
+	go func() {
+		signal.Notify(sigchan, os.Interrupt)
+		<-sigchan
+		log.Println("Считывание остановлено.")
+
+		err := f.Close()
+		if err != nil {
+			log.Printf("Ошибка при чтении строки %s\n", err)
+		}
+		close(readChan)
+		os.Exit(0)
+	}()
+
+	var a string
+	go FileWriter(readChan, f, mu)
+	for {
+		_, err := fmt.Scan(&a)
+		if err != nil {
+			log.Printf("Ошибка при чтении строки %s\n", err)
+		}
+		readChan <- a
 	}
 
 }
+
+func FileWriter(ch chan string, f *os.File, mu *sync.Mutex) {
+	for word := range ch {
+		mu.Lock()
+		_, err := f.WriteString(word + "\n")
+		if err != nil {
+			log.Printf("Ошибка при записи строки %s\n", err)
+		}
+		mu.Unlock()
+	}
+
+}
+
 
 /*1. Напишите 2 функции:
 	Первая функция читает ввод с консоли. Ввод одного значения заканчивается нажатием клавиши enter.
 	Вторая функция пишет эти данные в файл. Свяжите эти функции каналом.
 Работа приложения должна завершится при нажатии клавиш ctrl+c с кодом 0. */
 
-/*2. Напишите функцию разделения массива чисел на массивы простых и составных чисел.
-Для записи в массивы используйте два разных канала и горутины.
-Важно, чтобы были использованы владельцы каналов.*/
-
-/*3. Реализуйте функцию слияния двух каналов в один.*/
